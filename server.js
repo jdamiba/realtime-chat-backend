0be1147f54405f13cb9b8da88275be025dca03d0
Joseph Damiba <josephdamiba@Josephs-MacBook-Pro.local>
2024-09-09 21:48:40 -0700

    use websockets

--- server.js
+++ server.js
@@ -1,47 +1,78 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
+const WebSocket = require("ws");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+const wss = new WebSocket.Server({ server });
 
 const users = new Map();
 const messages = [];
 
-io.on("connection", (socket) => {
-  console.log("New client connected:", socket.id);
+function broadcast(message) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
 
-  socket.on("set username", (username) => {
-    users.set(socket.id, username);
-    console.log(`User ${socket.id} set username to ${username}`);
-    io.emit("user list", Array.from(users.values()));
-    socket.emit("chat history", messages);
+function sendToClient(client, message) {
+  if (client.readyState === WebSocket.OPEN) {
+    client.send(JSON.stringify(message));
+  }
+}
+
+wss.on("connection", (ws) => {
+  const clientId = Math.random().toString(36).substr(2, 9);
+  console.log("New client connected:", clientId);
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+
+    switch (data.type) {
+      case "set_username":
+        users.set(clientId, data.username);
+        console.log(`User ${clientId} set username to ${data.username}`);
+        broadcast({
+          type: "user_list",
+          users: Array.from(users.values()),
+        });
+        sendToClient(ws, {
+          type: "chat_history",
+          messages: messages,
+        });
+        break;
+
+      case "chat_message":
+        const username = users.get(clientId) || "Anonymous";
+        const messageToSend = {
+          text: data.text,
+          username: username,
+          timestamp: new Date().toISOString(),
+        };
+        messages.push(messageToSend);
+        broadcast({
+          type: "chat_message",
+          message: messageToSend,
+        });
+        break;
+
+      default:
+        console.log("Unknown message type:", data.type);
+    }
   });
 
-  socket.on("chat message", (msg) => {
-    const username = users.get(socket.id) || "Anonymous";
-    const messageToSend = {
-      text: msg,
-      username: username,
-      timestamp: new Date().toISOString(),
-    };
-    messages.push(messageToSend);
-    io.emit("chat message", messageToSend);
-  });
-
-  socket.on("disconnect", () => {
-    users.delete(socket.id);
-    console.log("Client disconnected:", socket.id);
-    io.emit("user list", Array.from(users.values()));
+  ws.on("close", () => {
+    users.delete(clientId);
+    console.log("Client disconnected:", clientId);
+    broadcast({
+      type: "user_list",
+      users: Array.from(users.values()),
+    });
   });
 });
 
